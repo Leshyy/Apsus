6b4639567bbdea7e84ae78c9803420facdae1d62
eranhh <eranhouja@gmail.com>
2020-12-27 05:38:27 +0200

    last changes last submission

--- apps/Notes/cmps/TodoListNote.jsx
+++ apps/Notes/cmps/TodoListNote.jsx
@@ -6,17 +6,13 @@ export class TodoListNote extends React.Component {
     let underline;
     if (isDone) underline='underline'
     else underline='none'
+    
     return (
       <div className="note-todos">
         <h4>{this.props.title}</h4>
-          {(todoList = todoList.map((todo, idx) => {
-            return (<li style={{textDecoration:underline}} onClick={() => {if(!isDone) {
-                  isDone=true
-                  }else{
-                    isDone=false
-                  }}}
-                  key={idx}>{todo}</li>)}))}
+          {todoList = todoList.map((todo, idx) => {
+            return <li key={idx}><input className="check-todos" type="checkbox"></input>{todo}</li>})}
       </div>
-    );
+    )
   }
 }
